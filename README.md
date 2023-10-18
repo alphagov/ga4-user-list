5a5eef3659e7a53b58ac8f630012a6c399ff3ea5
Ian Ansell <ian.ansell.uk@gmail.com>
2023-10-18 16:39:29 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # GA4_user_list
+This produces a list of all users with access to the same accounts/ GA4 properties as you and saves the data in BigQuery.
+
+This is designed to be run as a Google Cloud Function as a service account, remeber to give the service access access to the accounts / properties you want monitored.
+
+This only lists users that have been granted access directly, it does not list out inherited access, for example if access is granted via membership to a Marketing Platform Group.
+
+
+## Requirements
+
+
+*   [Google Cloud Platform (GCP) project](https://cloud.google.com/resource-manager/docs/creating-managing-projects) with [billing enabled](https://cloud.google.com/billing/docs/how-to/modify-project#enable-billing) - Create or use an existing project as needed.
+    *   Note: This solution uses billable GCP resources.
+*   [Google Analytics](https://analytics.google.com/analytics/web/)
+
+
+### Environment variables needed
+These can be set in a `.env` file
+`GCP_PROJECT_ID` The project to host the Cloud Function and the BigQuery data
+`GA4_ENTITY` in the format `properties/123456` or `accounts/123456`
---
+++ main.py
@@ -0,0 +1,59 @@
+from google.analytics.admin import AnalyticsAdminServiceClient
+from google.analytics import admin_v1alpha
+import pandas as pd
+import functions_framework
+import re
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+PROJECT = os.getenv('GCP_PROJECT_ID')
+analytics_client = AnalyticsAdminServiceClient()
+
+
+@functions_framework.http
+def list_users():
+    analytics_client = AnalyticsAdminServiceClient()
+    request = admin_v1alpha.ListAccountsRequest()
+    accounts = analytics_client.list_accounts(request=request)
+    user_list = []
+    for account in accounts:
+        users = analytics_client.list_access_bindings(parent=account.name)
+        for user in users:
+            email = user.user
+            binding = user.name
+            roles = user.roles
+            property_id = re.search(r'accounts\/\d*', binding)
+            data = {'email': email, 'binding': binding, 'roles': roles, 'parent': property_id.group()}
+            user_list.append(data)
+        request = admin_v1alpha.ListPropertiesRequest(
+            filter=f"parent:{account.name}",)
+        properties = analytics_client.list_properties(request=request)
+        for property in properties:
+            users = analytics_client.list_access_bindings(parent=property.name)
+            for user in users:
+                email = user.user
+                binding = user.name
+                roles = user.roles
+                property_id = re.search(r'properties\/\d*', binding)
+                data = {'email': email, 'binding': binding, 'roles': roles, 'parent': property_id.group()}
+                user_list.append(data)
+    df = pd.DataFrame(user_list, dtype='string')
+    table_id = 'user_admin.all_users'
+    df.to_gbq(table_id, project_id=PROJECT, if_exists='replace')
+    print("done")
+
+
+@functions_framework.http
+def run(request=None):
+    try:
+        list_users()
+        return "all good"
+    except Exception as e:
+        print(e)
+        return "all bad"
+
+
+if __name__ == '__main__':
+    run()
